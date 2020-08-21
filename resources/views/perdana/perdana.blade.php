8fa3abea01dc362ded4d15c23ff239477623107e
Your Name <you@example.com>
2020-08-21 18:54:45 +0700

    lumen_perdana

---
+++ resources/views/perdana/perdana.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Perdana</title>
+        <link rel="stylesheet" href="{{('css/perdana.css')}}">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+        <body>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container">
+                <img class="imgg1" src="./img/lg.png">
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav" >
+                <ul class="navbar-nav" style="margin-top: -35px; font-size: 20px; margin-left: 300px;">
+                  <!-- <li class="nav-item">
+                    <a class="navbar-brand" href="#" style="font-weight: bold; font-size: 12px;"></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="navbar-brand" href="#" style="font-weight: bold; font-size: 12px;"></a>
+                  </li> -->
+                   <li class="nav-item">
+                    <a class="navbar-brand" href="#" style="font-family: 'Roboto', sans-serif; font-weight: 700; font-size: 12px; margin-left: 250px;"> WhatsApp<img class="imgg2" src="./img/wa.png"> 0812 46666 968</a>
+                  </li>
+                </ul>
+
+                <ul class="navbar-nav" style="font-family: 'Fira Sans', sans-serif; font-weight: 500; margin-top: 30px; margin-left: -637px;">
+                    <li class="nav-item">
+                        <a class="navbar-brand nav-link" href="#" style=" font-size: 18px;">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="navbar-brand nav-link" href="#" style=" font-size: 18px;">About</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="navbar-brand nav-link" href="#" style=" font-size: 18px;">Products</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="navbar-brand nav-link" href="#" style=" font-size: 18px;">Industry Application</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="navbar-brand nav-link" href="#" style=" font-size: 18px;">Quality</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="navbar-brand nav-link" href="#" style=" font-size: 18px;">Contact</a>
+                      </li>
+                </ul>
+              </div>
+            </nav>        
+        <div class="konten">
+            <div class="container">
+                <h5 class="ctn" style="font-family: 'Fira Sans', sans-serif; padding-top: 180px; padding-bottom: 200px; color: white; font-weight: bold;">
+                    <p style="font-size: 33px;" class="t1">Your partner of chemicals and <br>
+                        fiberglass composite materials <br>
+                    <br></p>
+                    <p style="font-size: 17px; font-weight: 300; margin-top:-30px;" class="t2">
+                        Perdana Chemindo Perkasa id specialised in <br>
+                        supplying the best source of waste water and water <br>
+                        treatment chemicalsm general industrial chemicals, <br>
+                        fiberglass composite materials and fiberglass <br>
+                        project fabrications. <br> <br>
+                    </p> 
+                    <div class="btn-group dropright" style="background-color: rgb(215, 215, 241);">
+                      <button type="button" class="btn" aria-haspopup="true" aria-expanded="false">
+                        LEARN MORE
+                      </button>
+                    </div>
+                </h5>
+            </div>
+        </div>
+    </body>
+</html>
